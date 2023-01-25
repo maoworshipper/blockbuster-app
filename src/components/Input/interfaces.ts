bf6089b971930cee98db7eca7834fca9ae97bbc3
MauricioMartinez <maoworshipper@gmail.com>
2023-01-25 14:42:00 -0500

    Update Input Component

--- src/components/Input/interfaces.ts
+++ src/components/Input/interfaces.ts
@@ -1,6 +1,9 @@
 export interface InputProps {
     type: string;
     name: string;
+    label?: string;
     placeholder?: string;
     value?: string | number;
+    size?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
