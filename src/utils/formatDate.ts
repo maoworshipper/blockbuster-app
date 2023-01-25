869fae0cb7ae30cd0459c2413a214c8328f686f3
MauricioMartinez <maoworshipper@gmail.com>
2023-01-25 14:42:39 -0500

    Create formatDate utility

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,7 @@
+export const formatDate = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month}-${day}`;
+}
